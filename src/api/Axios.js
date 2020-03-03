de165a03813acfdce897f22a4fccf1eb96e254f1
emmavanninen <emmavanninen@gmail.com>
2020-03-03 15:58:37 -0500

    https fix

--- src/api/Axios.js
+++ src/api/Axios.js
@@ -3,6 +3,6 @@ import axios from 'axios'
 export const Axios = axios.create({
   baseURL:
     process.env.NODE_ENV !== 'production'
-      ? 'https://venue-booking-calendar-backend.herokuapp.com/'
+      ? 'http://venue-booking-calendar-backend.herokuapp.com/'
       : 'http://localhost:3001'
 })
